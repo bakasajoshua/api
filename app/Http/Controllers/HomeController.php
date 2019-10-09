5886d29e074dc50c42eb525b28d25553a15b5729
Joshua Bakasa <baksajoshua09@gmail.com>
2019-10-09 17:15:17 +0300

    Bug fix

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -29,19 +29,19 @@ class HomeController extends Controller
             if (!isset($this->testtypes[$testtype]))
                 goto next;
             $table = $this->testtypes[$testtype];
-            $return[$testtype]['data'] = DB::connection('national')->table($table)->join('view_facilitys', 'view_facilitys.id', '=', "{$table}.facility_id")->where('site_entry', '=', 2)->whereYear('datetested',$year)->groupBy('facilitycode')->when($request->has('facilitycode', function($query() use ($request){
-                    return $query->where('view_facilitys.facilitycode', $request->input('facilitycode'));
-                })))->when($request->has('month', function($query() use ($request){
+            $return[$testtype]['data'] = DB::connection('national')->table($table)->join('view_facilitys', 'view_facilitys.id', '=', "{$table}.facility_id")->where('site_entry', '=', 2)->whereYear('datetested',$year)->groupBy('facilitycode')->when($request->has('facilitycode'), function($query) use ($request){
+                        return $query->where('view_facilitys.facilitycode', $request->input('facilitycode'));
+                    })->when($request->has('month'), function($query) use ($request){
                         return $query->whereMonth('datetested', $request->input('month'));
-                })))->groupBy('facilityname')->orderBy('tests', 'desc')->selectRaw("view_facilitys.facilitycode,view_facilitys.name AS `facilityname`,COUNT($table.id) AS `tests`")->get();
+                    })->groupBy('facilityname')->orderBy('tests', 'desc')->selectRaw("view_facilitys.facilitycode,view_facilitys.name AS `facilityname`,COUNT($table.id) AS `tests`")->get();
         } else {
             next:
             foreach ($this->testtypes as $key => $value) {
-                $return[$key]['data'] = DB::connection('national')->table($value)->join('view_facilitys', 'view_facilitys.id', '=', "{$value}.facility_id")->where('site_entry', '=', 2)->whereYear('datetested',$year)->when($request->has('facilitycode', function($query() use ($request){
+                $return[$key]['data'] = DB::connection('national')->table($value)->join('view_facilitys', 'view_facilitys.id', '=', "{$value}.facility_id")->where('site_entry', '=', 2)->whereYear('datetested',$year)->when($request->has('facilitycode'), function($query) use ($request){
                         return $query->where('view_facilitys.facilitycode', $request->input('facilitycode'));
-                    })))->when($request->has('month', function($query() use ($request){
+                    })->when($request->has('month'), function($query) use ($request){
                         return $query->whereMonth('datetested', $request->input('month'));
-                    })))->groupBy('facilitycode')->groupBy('facilityname')->orderBy('tests', 'desc')->selectRaw("view_facilitys.facilitycode,view_facilitys.name AS `facilityname`,COUNT({$value}.id) AS `tests`")->get();
+                    })->groupBy('facilitycode')->groupBy('facilityname')->orderBy('tests', 'desc')->selectRaw("view_facilitys.facilitycode,view_facilitys.name AS `facilityname`,COUNT({$value}.id) AS `tests`")->get();
                 $return[$key]['test'] = $key;
                 $return[$key]['year'] = $year;
                 if ($request->has('month'))
